bfd31b5fba11756b56bd610d59dcf437f1b23204
Rodrigo <rostudies2@gmail.com>
2025-02-16 13:43:31 -0300

    feat: implement random draw functionality and improve layout
    - Add reveal friend button and random selection logic
    - Improve layout spacing and alignment
    - Refactor classes and IDs for better readability

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 let friendsList = []
 
+function displayTextOnScreen(tag, text) {
+    let element = document.querySelector(tag);
+    element.innerHTML = text;
+}
+
 function insertFriend() {
-    let nameInput = document.getElementById('inputName')
-    if (nameInput.value.trim() === '') {
-        alert('Por favor, insira um nome')
+    let inputName = document.getElementById('inputField')
+    if (inputName.value.trim() === '') {
+        displayTextOnScreen(
+            '.alert-message',
+            'Por favor, insira um nome'
+        );
     } else {
-        friendsList.push(nameInput.value)
-        nameInput.value = ''
+        friendsList.push(inputName.value)
+        inputName.value = ''
         console.log(friendsList)
         updateFriendsList()
     }
 }
 
 function updateFriendsList() {
-    let list = document.getElementById('namesList'); 
+    let list = document.getElementById('participantsList'); 
     list.innerHTML = ''; 
     for (let i = 0; i < friendsList.length; i++) { 
-        let listaItem = document.createElement('li'); 
-        listaItem.textContent = friendsList[i]; 
-        list.appendChild(listaItem); 
+        let listItem = document.createElement('li'); 
+        listItem.textContent = friendsList[i]; 
+        list.appendChild(listItem); 
     }
 }
+
+function drawRandomParticipant() { 
+    if (friendsList.length < 2) {
+        displayTextOnScreen(
+            '.alert-message',
+            'Por favor, adicione mais amigos'
+        );
+    } else {
+        let pickRandomFriend = friendsList[Math.floor(Math.random() * friendsList.length)];
+        displayTextOnScreen(
+            '#drawResult', 
+            `Seu amigo secreto é: ${pickRandomFriend}`
+        );
+    }
+}
+
+/*         let result = document.getElementById('drawResult');
+        result.textContent = pickRandomFriend; */
